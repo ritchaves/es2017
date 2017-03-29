3f8c68f0c012716526c1775c055828ad7c26e147
nunocartier <soarestomas@yahoo.com.br>
2017-03-29 14:09:42 +0100

    Cenas no process

--- broker/src/main/java/pt/ulisboa/tecnico/softeng/broker/domain/Adventure.java
+++ broker/src/main/java/pt/ulisboa/tecnico/softeng/broker/domain/Adventure.java
@@ -157,6 +157,7 @@ public class Adventure {
 	public State getState() {
 		switch (this.oldState) {
 		case PROCESS_PAYMENT:
+			return this.state.getState();
 		case RESERVE_ACTIVITY:
 		case BOOK_ROOM:
 		case UNDO:
@@ -174,7 +175,7 @@ public class Adventure {
 		this.oldState = state;
 		switch (state) {
 		case PROCESS_PAYMENT:
-			this.state = null;
+			this.state = new ProcessPaymentState();
 			break;
 		case RESERVE_ACTIVITY:
 			this.state = null;
@@ -203,20 +204,7 @@ public class Adventure {
 
 		switch (this.oldState) {
 		case PROCESS_PAYMENT:
-			try {
-				this.paymentConfirmation = BankInterface.processPayment(this.IBAN, this.amount);
-			} catch (BankException be) {
-				setState(State.CANCELLED);
-			} catch (RemoteAccessException rae) {
-				// increment number of errors
-				// if (number of errors == 3) {
-				// setState(State.CANCELLED);
-				// }
-				return;
-			}
-
-			setState(State.RESERVE_ACTIVITY);
-
+			this.state.process(this);
 			break;
 		case RESERVE_ACTIVITY:
 			try {
