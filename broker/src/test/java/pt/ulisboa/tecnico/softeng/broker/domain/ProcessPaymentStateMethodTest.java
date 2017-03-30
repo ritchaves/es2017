77d919c0fab9cbc5c8e6f524f1ba4dca96bc2846
nunocartier <soarestomas@yahoo.com.br>
2017-03-30 14:10:38 +0100

    ProcessPayment tests com erros

---
+++ broker/src/test/java/pt/ulisboa/tecnico/softeng/broker/domain/ProcessPaymentStateMethodTest.java
@@ -0,0 +1,137 @@
+package pt.ulisboa.tecnico.softeng.broker.domain;
+
+import org.joda.time.LocalDate;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import mockit.Expectations;
+import mockit.Injectable;
+import mockit.Mocked;
+import mockit.integration.junit4.JMockit;
+import pt.ulisboa.tecnico.softeng.bank.dataobjects.BankOperationData;
+import pt.ulisboa.tecnico.softeng.bank.exception.BankException;
+import pt.ulisboa.tecnico.softeng.broker.domain.Adventure.State;
+import pt.ulisboa.tecnico.softeng.broker.exception.RemoteAccessException;
+import pt.ulisboa.tecnico.softeng.broker.interfaces.ActivityInterface;
+import pt.ulisboa.tecnico.softeng.broker.interfaces.BankInterface;
+import pt.ulisboa.tecnico.softeng.broker.interfaces.HotelInterface;
+	
+@RunWith(JMockit.class)
+public class ProcessPaymentStateMethodTest {
+	private static final String IBAN = "BK01987654321";
+	private static final int AMOUNT = 300;
+	private static final String PAYMENT_CONFIRMATION = "PaymentConfirmation";
+	private static final String PAYMENT_CANCELLATION = "PaymentCancellation";
+	private static final String ACTIVITY_CONFIRMATION = "ActivityConfirmation";
+	private static final String ACTIVITY_CANCELLATION = "ActivityCancellation";
+	private static final String ROOM_CONFIRMATION = "RoomConfirmation";
+	private static final String ROOM_CANCELLATION = "RoomCancellation";
+	private static int errors;
+	private final LocalDate begin = new LocalDate(2016, 12, 19);
+	private final LocalDate end = new LocalDate(2016, 12, 21);
+	private Adventure adventure;
+
+	@Injectable
+	private Broker broker;
+
+	@Before
+	public void setUp() {
+		this.adventure = new Adventure(this.broker, this.begin, this.end, 20, IBAN, 300);
+		this.adventure.setState(State.PROCESS_PAYMENT);
+	}
+
+	@Test
+	public void readyToReserve() {
+		this.adventure.process();
+
+		Assert.assertEquals(Adventure.State.RESERVE_ACTIVITY, this.adventure.getState());
+	}
+
+	@Test
+	public void processPaymentFirstBankException(@Mocked final BankInterface bankInterface) {
+		this.adventure.getIBAN();
+		this.adventure.getAmount();
+
+		new Expectations() {
+			{
+				BankInterface.processPayment(IBAN , AMOUNT);
+				this.result = new BankException();
+				Assert.assertEquals(Adventure.State.CANCELLED, this.adventure.getState());
+			}
+		};
+
+		this.adventure.process();
+
+		Assert.assertEquals(Adventure.State.RESERVE_ACTIVITY, this.adventure.getState());
+	}
+
+	@Test
+	public void processPaymentFirstRemoteAccessException(@Mocked final BankInterface bankInterface) {
+		this.adventure.getIBAN();
+		this.adventure.getAmount();
+
+		new Expectations() {
+			{
+				BankInterface.processPayment(IBAN , AMOUNT);
+				this.result = new RemoteAccessException();
+			}
+		};
+
+		this.adventure.process();
+
+		Assert.assertEquals(Adventure.State.RESERVE_ACTIVITY, this.adventure.getState());
+	}
+
+	@Test
+	public void processPaymentNumberErrorsException(@Mocked final BankInterface bankInterface) {
+		this.adventure.getIBAN();
+		this.adventure.getAmount();
+
+		new Expectations() {
+			{	
+				errors = AdventureState.getNumOfRemoteErrors();
+				while (errors < 3){
+					BankInterface.processPayment(IBAN , AMOUNT);
+					this.result = new RemoteAccessException();
+					errors += AdventureState.incNumOfRemoteErrors();
+				}
+				if (errors == 3){
+					Assert.assertEquals(Adventure.State.CANCELLED, this.adventure.getState());
+				}
+			}
+		};
+
+		this.adventure.process();
+
+		Assert.assertEquals(Adventure.State.RESERVE_ACTIVITY, this.adventure.getState());
+	}
+
+
+	@Test
+	public void cancelledRoom(@Mocked final BankInterface bankInterface,
+			@Mocked final ActivityInterface activityInterface, @Mocked final HotelInterface hotelInterface) {
+		this.adventure.setPaymentConfirmation(PAYMENT_CONFIRMATION);
+		this.adventure.setPaymentCancellation(PAYMENT_CANCELLATION);
+		this.adventure.setActivityConfirmation(ACTIVITY_CONFIRMATION);
+		this.adventure.setActivityCancellation(ACTIVITY_CANCELLATION);
+		this.adventure.setRoomConfirmation(ROOM_CONFIRMATION);
+		this.adventure.setRoomCancellation(ROOM_CANCELLATION);
+
+		new Expectations() {
+			{
+				BankInterface.getOperationData(PAYMENT_CANCELLATION);
+
+				ActivityInterface.getActivityReservationData(ACTIVITY_CANCELLATION);
+
+				HotelInterface.getRoomBookingData(ROOM_CANCELLATION);
+			}
+		};
+
+		this.adventure.process();
+
+		Assert.assertEquals(Adventure.State.CANCELLED, this.adventure.getState());
+	}
+
+}
