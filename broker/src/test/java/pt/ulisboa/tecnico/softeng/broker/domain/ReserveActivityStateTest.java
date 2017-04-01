5546e216420547db575c997de9f947bcbc5f40e5
David Cardoso <davidanil@hotmail.com>
2017-04-01 14:35:40 +0100

    #8 need someone to check tests, why are they wrong?

--- broker/src/test/java/pt/ulisboa/tecnico/softeng/broker/domain/ReserveActivityStateTest.java
+++ broker/src/test/java/pt/ulisboa/tecnico/softeng/broker/domain/ReserveActivityStateTest.java
@@ -5,12 +5,17 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
+import mockit.Expectations;
 import mockit.Injectable;
 import mockit.Mocked;
 import mockit.integration.junit4.JMockit;
+import pt.ulisboa.tecnico.softeng.activity.exception.ActivityException;
 import pt.ulisboa.tecnico.softeng.broker.domain.Adventure.State;
+import pt.ulisboa.tecnico.softeng.broker.exception.RemoteAccessException;
 import pt.ulisboa.tecnico.softeng.broker.interfaces.ActivityInterface;
+import pt.ulisboa.tecnico.softeng.broker.interfaces.HotelInterface;
+import pt.ulisboa.tecnico.softeng.hotel.domain.Room;
+
 
 @RunWith(JMockit.class)
 public class ReserveActivityStateTest {
@@ -44,5 +49,93 @@ public class ReserveActivityStateTest {
 		this.adventure.process();
 
 		Assert.assertEquals(Adventure.State.BOOK_ROOM, this.adventure.getState());
+	}
+	
+	@Test
+	public void reserveActivityException(@Mocked final ActivityInterface activityInterface) {
+		this.adventure.getEnd();
+		this.adventure.getBegin();
+		this.adventure.getAge();
+		
+		new Expectations() {
+			{
+				ActivityInterface.reserveActivity(begin, end, age);
+				this.result = new ActivityException();
+			}
+		};
+		
+		this.adventure.process();
+		Assert.assertEquals(Adventure.State.UNDO, this.adventure.getState());
+	}
+	
+	@Test
+	public void reserve5Exception(@Mocked final ActivityInterface activityInterface) {
+		this.adventure.getBegin();
+		this.adventure.getEnd();
+		this.adventure.getAge();
+		
+		new Expectations() {
+			{
+				ActivityInterface.reserveActivity(begin, end, age);
+				this.result = new RemoteAccessException();
+				ActivityInterface.reserveActivity(begin, end, age);
+				this.result = new RemoteAccessException();
+				ActivityInterface.reserveActivity(begin, end, age);
+				this.result = new RemoteAccessException();
+				ActivityInterface.reserveActivity(begin, end, age);
+				this.result = new RemoteAccessException();
+				ActivityInterface.reserveActivity(begin, end, age);
+				this.result = new RemoteAccessException();
+
+			}
+		};
+		
+		for (int i = 0; i < 5; i ++) this.adventure.process();
+		
+		Assert.assertEquals(Adventure.State.UNDO, this.adventure.getState());
+	}
+	
+	@Test
+	public void activityLessThan5Exception(@Mocked final ActivityInterface activityInterface) {
+		this.adventure.getBegin();
+		this.adventure.getEnd();
+		this.adventure.getAge();
+		
+		new Expectations() {
+			{
+				ActivityInterface.reserveActivity(begin, end, age);
+				this.result = new RemoteAccessException();
+			}
+		};
+		
+		this.adventure.process();
+		Assert.assertEquals(Adventure.State.RESERVE_ACTIVITY, this.adventure.getState());
+	}
+	
+	@Test
+	public void activityMoreThan5Exception(@Mocked final ActivityInterface activityInterface) {
+		this.adventure.getBegin();
+		this.adventure.getEnd();
+		this.adventure.getAge();
+		
+		new Expectations() {
+			{
+				ActivityInterface.reserveActivity(begin, end, age);
+				this.result = new RemoteAccessException();
+				ActivityInterface.reserveActivity(begin, end, age);
+				this.result = new RemoteAccessException();
+				ActivityInterface.reserveActivity(begin, end, age);
+				this.result = new RemoteAccessException();
+				ActivityInterface.reserveActivity(begin, end, age);
+				this.result = new RemoteAccessException();
+				ActivityInterface.reserveActivity(begin, end, age);
+				this.result = new RemoteAccessException();
+				ActivityInterface.reserveActivity(begin, end, age);
+				this.result = new RemoteAccessException();
+			}
+		};
+		for (int i = 0; i < 6; i ++)
+			this.adventure.process();
+		Assert.assertEquals(Adventure.State.CANCELLED, this.adventure.getState());
 	}
 }
