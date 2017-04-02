1589c1b5580336b1c8bd9f82d746b332f455426c
ritchaves <rita.c.ferreira@live.com>
2017-04-02 16:53:40 +0100

    test bank.cancelpayment finish close #15

--- bank/src/test/java/pt/ulisboa/tecnico/softeng/bank/domain/BankCancelPayment.java
+++ bank/src/test/java/pt/ulisboa/tecnico/softeng/bank/domain/BankCancelPayment.java
@@ -5,11 +5,13 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import pt.ulisboa.tecnico.softeng.bank.domain.Operation.Type;
 import pt.ulisboa.tecnico.softeng.bank.exception.BankException;
 
 public class BankCancelPayment {
 	private Bank bank;
 	private Account account;
+	private Operation operation;
 
 	@Before
 	public void setUp() {
@@ -17,18 +19,26 @@ public class BankCancelPayment {
 		Client client = new Client(this.bank, "M");
 		this.account = new Account(this.bank, client);
 		this.account.deposit(100);
+		this.operation = new Operation(Type.WITHDRAW, this.account, 20);
 	}
 
-//	public static String cancelPayment(String reference) {
-//		for (Bank bank : Bank.banks) {
-//			if(bank.getOperation(reference) != null) {
-//				Operation canceledOperation = bank.getOperation(reference);
-//				Account creditAcc = canceledOperation.getAccount();
-//				return creditAcc.deposit(canceledOperation.getValue());
-//			}
-//		}
-//		throw new BankException();
-//	}
+	@Test
+	public void success() {
+		Bank.cancelPayment(this.operation.getReference());
+		Assert.assertEquals(120, this.account.getBalance());	
+	}
+	
+	@Test
+	public void notSucess() {
+		Bank.cancelPayment(this.operation.getReference());
+		Assert.assertNotEquals(10, this.account.getBalance());
+	}
+	
+	@Test
+	public void notSucess2() {
+		Bank.cancelPayment(this.operation.getReference());
+		Assert.assertNotEquals(140, this.account.getBalance());
+	}
 	
 	@Test(expected = BankException.class)
 	public void referenceNull() {
@@ -44,7 +54,6 @@ public class BankCancelPayment {
 	public void referenceDoesNotExist() {
 		Bank.cancelPayment("XPTO");
 	}
-	
 	
 	@After
 	public void tearDown() {
