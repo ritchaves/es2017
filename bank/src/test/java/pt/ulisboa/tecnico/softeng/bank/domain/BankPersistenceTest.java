999ec324448ddf2c38cd06b68cd67e6b267d68c3
David Cardoso <davidanil@hotmail.com>
2017-04-23 18:15:20 +0100

    close #67

--- bank/src/test/java/pt/ulisboa/tecnico/softeng/bank/domain/BankPersistenceTest.java
+++ bank/src/test/java/pt/ulisboa/tecnico/softeng/bank/domain/BankPersistenceTest.java
@@ -41,22 +41,43 @@ public class BankPersistenceTest{
 			operation = op;
 		}
 		
-		// Test percistency of Bank
+		Account account = null;
+		for (Account acc : bank.getAccountSet()){
+			account = acc;
+		}
 		
+		Client client = null;
+		for (Client cli : bank.getClientSet()){
+			client = cli;
+		}
+		
+		// Test persistence of Bank
 		Assert.assertEquals(operation, bank.getOperation(operation.getReference()));
-		//Assert.assertEquals(account, bank.getAccount(account.getIBAN()));		SAME HERE DAVID
-		//Assert.assertTrue(bank.hasClient(client));		SAME HERE DAVID
+		Assert.assertEquals(account, bank.getAccount(account.getIBAN()));
+		Assert.assertTrue(bank.hasClient(client));	
 		
-		// Test percistency of Operation
-		
+		// Test persistence of Operation
 		Assert.assertNotNull(operation);
 		Assert.assertTrue(operation.getReference().startsWith(bank.getCode()));
 		Assert.assertTrue(operation.getReference().length() > Bank.CODE_SIZE);
 		Assert.assertEquals(Type.DEPOSIT, operation.getType());
-//		Assert.assertEquals(account, operation.getAccount());  DAVID QUANDO FIZERES O ACCOUNT COM NOME "ACCOUNT" TIRA ISTO DE COMENTARIO"
+		Assert.assertEquals(account, operation.getAccount()); 
 		Assert.assertEquals(1000, operation.getValue());
 		Assert.assertTrue(operation.getTime() != null);
 		Assert.assertEquals(bank, operation.getBank());
+		
+		//Test persistence of Client
+		Assert.assertNotNull(client);
+		Assert.assertEquals(CLIENT_NAME, client.getName());
+		Assert.assertEquals(bank, client.getBank());
+		Assert.assertNotNull(client.getID()); 
+		
+		//Test persistence of Account
+		Assert.assertNotNull(account);
+		Assert.assertTrue(account.getIBAN().startsWith(BANK_CODE));
+		Assert.assertEquals(0, account.getBalance());
+		Assert.assertEquals(bank, account.getBank());
+		Assert.assertEquals(client, account.getClient());
 		
 	}
 	
