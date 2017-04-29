ad817a282bf10670dce89953370323aea767adfe
Diogo Freitas <diogo.maria.freitas@gmail.com>
2017-04-29 13:00:56 +0100

    Broker Code Refactorization to Bank

---
+++ bank/src/main/java/pt/ulisboa/tecnico/softeng/bank/remote/HotelInterface.java
@@ -0,0 +1,33 @@
+package pt.ulisboa.tecnico.softeng.bank.remote;
+
+import java.util.Set;
+
+import org.joda.time.LocalDate;
+
+import pt.ulisboa.tecnico.softeng.bank.services.remote.dataobjects.RoomBookingData;
+
+public class HotelInterface {
+	public static enum RoomType {
+		SINGLE, DOUBLE
+	}
+
+	public static String reserveRoom(RoomType type, LocalDate arrival, LocalDate departure) {
+		// TODO: implement in the final version as a rest invocation
+		return null;
+	}
+
+	public static String cancelBooking(String roomConfirmation) {
+		// TODO: implement in the final version as a rest invocation
+		return null;
+	}
+
+	public static RoomBookingData getRoomBookingData(String reference) {
+		// TODO: implement in the final version as a rest invocation
+		return null;
+	}
+
+	public static Set<String> bulkBooking(int number, LocalDate arrival, LocalDate departure) {
+		// TODO: implement in the final version as a rest invocation
+		return null;
+	}
+}
